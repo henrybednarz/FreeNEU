c5b86522eb05faf60622cd76e7a3253190174460
henrybednarz <bednarz.h@northeastern.edu>
2025-11-28 16:56:12 -0500

    file cache overlap

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const withSerwist = withSerwistInit({
     swSrc: path.join(__dirname, 'src/app/sw.js'),
     swDest: 'public/sw.js',
     disable: process.env.NODE_ENV === 'development',
+    exclude: [
+        /marker-icon.*?\.png$/,
+        /marker-shadow.*?\.png$/
+    ],
 });
 
 /** @type {import('next').NextConfig} */
